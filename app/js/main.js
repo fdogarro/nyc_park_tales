7745cf3407277c3cfc0be97c9761c4468e201925
fdw101 <felicia.ogarro@gmail.com>
2014-07-29 22:25:55 -0400

    starter

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-	$('p:contains("<p>")').html('');
-	});
-	
+// $(document).ready(function(){
+// 	var char = 50;
+// 	var endText = "...";
+// 	var moreText = " read more";
+// 	var lessText = "less";
 
+// 	$('.more').each(function(){
+// 		var content = $(this).html();
+// 		if(content.length > char){
+// 			var 
+// 		}
+// 	});
 // });
+	
